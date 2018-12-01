02548e403b585fecb283cf2d4076a2689f4e64dc
Antoine <antoine.sb@orange.fr>
2018-12-01 17:04:49 +0000

    [UPDATE] gitignore; cargo.toml + [WIP] main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::env;
+use std::process;
 
-//https://doc.rust-lang.org/book/second-edition/ch11-01-writing-tests.html 
-
+// cls && cargo build && cargo run
+/*
+ * Test the project.
+ */
 #[cfg(test)]
 mod tests {
     #[test]
@@ -10,6 +14,33 @@ mod tests {
     }
 }
 
+/*
+ * If arguments are found, the program exit, otherwise print a sentence.
+ */
+fn check_args() {
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        1 => println!("The world is grey, the mountains old. The forge's fire is ashen cold."),
+        _ => {
+            eprintln!("This program does not take arguments.");
+            process::exit(1);
+        },
+    } 
+}
+
+fn display_world(&world: &[[bool; 20]; 20]) {
+    for &element in world.iter() {
+        println!("row");
+    }
+}
+
+/*
+ * Entry point.
+ */
 fn main() {
-    println!("Hello, world!");
+    check_args();
+    let mut world: [[bool; 20]; 20] = [[false; 20]; 20];
+    world[0][0] = true;
+    display_world(&world);
 }
